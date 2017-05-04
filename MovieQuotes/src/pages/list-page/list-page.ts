29ec9932681df50ef9fc2c17be99a0393d7624fa
Donald Sisco <dododonnars@gmail.com>
2017-05-04 09:50:29 -0400

    add quote dialog

--- MovieQuotes/src/pages/list-page/list-page.ts
+++ MovieQuotes/src/pages/list-page/list-page.ts
@@ -1,6 +1,7 @@
+import { MovieQuote } from './../../models/MovieQuote.model';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { MovieQuote } from "../../models/MovieQuote.model";
 
 /**
@@ -17,12 +18,42 @@ import { MovieQuote } from "../../models/MovieQuote.model";
 export class ListPage {
   public movieQuotesStream: FirebaseListObservable<MovieQuote[]>
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private afDatabase: AngularFireDatabase) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private afDatabase: AngularFireDatabase, private alertCrlr: AlertController) {
     this.movieQuotesStream = this.afDatabase.list("Quotes")
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ListPage');
+  }
+
+  addQuote(): void {
+    const prompt = this.alertCrlr.create({
+      title: "Add Quote",
+      inputs: [
+        {
+          name: 'quote',
+          placeholder: 'Quote',
+        },
+        {
+          name: 'movie',
+          placeholder: 'Quote from'
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          handler: () => {
+          }
+        },
+        {
+          text: "Add Quote",
+          handler: (data: MovieQuote) => {
+            console.log(data)
+          }
+        }
+      ]
+    })
+    prompt.present()
   }
 
 }
