809b88a93715c3d3ef0db4a224799904c489e4fb
Donald Sisco <dododonnars@gmail.com>
2017-05-04 09:16:28 -0400

    Check in

--- MovieQuotes/src/app/app.module.ts
+++ MovieQuotes/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { ListPageModule } from './../pages/list-page/list-page.module';
+import { QuoteDetailPageModule } from './../pages/quote-detail-page/quote-detail-page.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -5,21 +7,20 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    QuoteDetailPageModule,
+    ListPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [
     StatusBar,
